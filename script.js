613465c393ffe57521d638e998697a4712d20461
angelbarros14 <social.angelbarros@gmail.com>
2025-02-27 21:53:26 +0800

    added layout and design of calculator

---
+++ script.js
@@ -0,0 +1,21 @@
+function add () {
+    number + number;
+
+}
+
+function subtract () {
+
+
+}
+
+function multiply () {
+
+}
+
+function divide () {
+
+}
+
+function operate (operator, number1, number2) {
+
+}
